eaaee32b13ac875260f572be020ac6b249297cb5
ZalgoSoft <microfool000@gmail.com>
2021-03-20 00:07:10 +0800

    more editors

---
+++ Editor/ZCombineChildrenMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class ZCombineChildrenMesh : MonoBehaviour
+{
+    public void doCombine()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        HashSet<CombineInstance> ci = new HashSet<CombineInstance>();
+        foreach (MeshFilter item in meshFilters)
+            if (item.sharedMesh)
+                ci.Add(new CombineInstance
+                {
+                    mesh = item.sharedMesh,
+                    transform = item.transform.localToWorldMatrix
+                });
+        Mesh mm = new Mesh { name = "CombinedMeshTemp" };
+        mm.CombineMeshes(ci.ToArray());
+        transform.GetComponent<MeshFilter>().sharedMesh = mm;
+
+        MeshRenderer[] mrs = GetComponentsInChildren<MeshRenderer>();
+        MeshRenderer mr = GetComponentInChildren<MeshRenderer>();
+        HashSet<Material> mats = new HashSet<Material>();
+        foreach (MeshRenderer item1 in mrs)
+            foreach (Material item2 in item1.sharedMaterials)
+                if (item2)
+                    mats.Add(item2);
+        mr.sharedMaterials = mats.ToArray();
+    }
+    [CustomEditor(typeof(ZCombineChildrenMesh))]
+    class ZCombineChildrenMeshEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (DrawDefaultInspector())
+            { }
+            if (GUILayout.Button("Combine meshes and materials"))
+            {
+                ((ZCombineChildrenMesh)target).doCombine();
+            }
+        }
+    }
+}
---
+++ Editor/ZUintyReflectorWindow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+[AttributeUsage(AttributeTargets.All)]
+public class ZUnityReflectorAttribute : Attribute
+{
+    private string myName;
+    public ZUnityReflectorAttribute(string name) { myName = name; }
+    public string Name { get { return myName; } }
+}
+public class ZUintyReflectorWindow : EditorWindow
+{
+    string path = String.Empty;
+    string text = String.Empty;
+    string output = String.Empty;
+    bool isTextUpdated = false;
+    Vector2 scroll;
+    [MenuItem("Window/Unity Assembly Reflector")]
+    static void Init()
+    {
+        ZUintyReflectorWindow window = (ZUintyReflectorWindow)EditorWindow.GetWindow(typeof(ZUintyReflectorWindow));
+        window.titleContent = new GUIContent("Reflector");
+        EditorStyles.textArea.wordWrap = true;
+        window.Show();
+    }
+    void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Open..."))
+        {
+            path = EditorUtility.OpenFilePanel("Open assembly file", "", "dll");
+            text = path;
+        }
+        if (GUILayout.Button("Show") && path != String.Empty)
+        {
+            text = "Listing contents of assembly " + path + "\n";
+            Assembly a = Assembly.LoadFile(path);
+            Type[] types = a.GetTypes();
+            foreach (Type t in types)
+            {
+                text += t.MemberType + ": " + t + "\n"; ;
+                MemberInfo[] mbrInfoArray = t.GetMembers();
+                foreach (MemberInfo mbrInfo in mbrInfoArray)
+                {
+                    object[] attrs = mbrInfo.GetCustomAttributes(false);
+                    if (attrs.Length > 0)
+                    {
+                        foreach (object o in attrs)
+                            text += o + "\n"; ;
+                    }
+                    text += mbrInfo.MemberType + ": " + mbrInfo + "\n"; ;
+                }
+            }
+            text += types.Length + " types found\n";
+            isTextUpdated = true;
+        }
+        EditorGUILayout.EndHorizontal();
+        scroll = EditorGUILayout.BeginScrollView(scroll);
+        output = EditorGUILayout.TextArea(text, EditorStyles.textArea, GUILayout.ExpandHeight(true));
+        EditorGUILayout.EndScrollView();
+        isTextUpdated = false;
+        EditorGUILayout.EndVertical();
+    }
+}

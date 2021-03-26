523847f8d92402a3d35e7488348be6206594d20b
ZalgoSoft <microfool000@gmail.com>
2021-03-27 06:45:55 +0800

    3d game platformer v3

---
+++ Scripts/3DPlatformer2/Scripts/BoxTileGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTileGenerator : MonoBehaviour
+{
+    public GameObject tBlock;
+     GameObject tParent;
+    GameObject tInstance;
+    public int xSize = 10;
+    public int ySize = 4;
+    public int zSize = 4;
+    public float spaceRatio = 1.2f;
+    public void Generate()
+    {
+        //if (tParent == null)
+        //{
+            tParent = new GameObject("container");
+            tParent.transform.position = Vector3.zero;
+        //}
+
+        for (float x = 0; x < xSize; ++x)
+            for (float y = 0; y < ySize; ++y)
+                for (float z = 0; z < xSize; ++z)
+                {
+                    tInstance = Instantiate(tBlock, new Vector3(x * spaceRatio, y * spaceRatio + 0.5f, z * spaceRatio), Quaternion.identity);
+                    tInstance.transform.parent = tParent.transform;
+                }
+    }
+}
+[CustomEditor(typeof(BoxTileGenerator))]
+class BoxTileGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var gen = (BoxTileGenerator)target;
+        if (DrawDefaultInspector())
+        {
+        }
+        if (GUILayout.Button("Generate"))
+        {
+            gen.Generate();
+        }
+    }
+}
---
+++ Scripts/3DPlatformer2/Scripts/DamageController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+public class DamageController : MonoBehaviour
+{
+    public float Health = 15f;
+    private Animator animator;
+    private Material material;
+    public GameObject boxdestroy;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        material = GetComponent<Renderer>().material;
+    }
+    public void takeDamage(float takenDamage, Vector3 force, Vector3 position)
+    {
+
+        if (TryGetComponent<Rigidbody>(out Rigidbody optionalRigidbody))
+            optionalRigidbody.AddForceAtPosition(force, position, ForceMode.Force);
+        Health -= takenDamage;
+        material.color = new Color(material.color.r + 0.05f, material.color.g, material.color.b);
+        if (Health <= 0f)
+            Die();
+        else
+            animator.SetTrigger("isHit");
+    }
+    void Die()
+    {
+        GameObject g = Instantiate(boxdestroy, transform.position, transform.rotation);
+        g.transform.parent = gameObject.transform.parent;
+        //Destroy(g, 1f);
+        Destroy(gameObject);
+    }
+}
